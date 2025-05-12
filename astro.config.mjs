8f8dd8aa5bd7d0a37bfb22752027b63a1bb6f6da
Samuel <goodlifesam@gmail.com>
2025-05-12 12:08:54 -0700

    something

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,26 @@
+// import { defineConfig } from "astro/config";
+// import sitemap from "@astrojs/sitemap";
+// import icon from "astro-icon";
+
+// export default defineConfig({
+//   site: "https://www.yourwebsite.com", // update me!
+//   integrations: [
+//     icon(),
+//     sitemap({
+//       filter: (page) => !page.includes("/admin"),
+//       changefreq: "weekly",
+//       priority: 0.7,
+//     }),
+//   ],
+// });
+
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   site: "https://www.yourwebsite.com", // update me!
@@ -12,4 +32,12 @@ export default defineConfig({
       priority: 0.7,
     }),
   ],
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+      }
+    }
+  }
 });
